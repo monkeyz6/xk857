68e672262b157e59f32ac832e5268f0355ce9e27
monkeyz6 <69353444+monkeyz6@users.noreply.github.com>
2022-08-02 14:59:19 +0800

    打包部署

--- docs/.vuepress/nav.js
+++ docs/.vuepress/nav.js
@@ -3,7 +3,7 @@ module.exports = [
   {
     text: 'Java',icon: 'iconfont icon-JavaCorefenxigongju', items: [
       {text: 'java开发技巧',icon: 'iconfont icon-java', link: '/后端开发/java/java高效编程常用技术优化手段/资源关闭优化方案'},
-      {text: '中间件/框架',icon: 'iconfont icon-xiaoxiduilie', link: '/后端开发/中间件/RabbitMQ/主流的消息队列和选择问题'},
+      {text: '消息队列专栏',icon: 'iconfont icon-xiaoxiduilie', link: '/后端开发/中间件/RabbitMQ/主流的消息队列和选择问题'},
       {text: 'Redis专栏',icon: 'iconfont icon-Redis', link: '/后端开发/redis专栏/高阶使用技巧/Redis概述及基本使用'},
     ]
   },
