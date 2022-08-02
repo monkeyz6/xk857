2c0f5ee0e416bfb6b049a7bd9c2e615e37e98066
monkeyz6 <69353444+monkeyz6@users.noreply.github.com>
2022-08-02 21:51:12 +0800

    setup语法糖之reactive

--- docs/前端开发/vue/sidebar.js
+++ docs/前端开发/vue/sidebar.js
@@ -5,7 +5,7 @@ module.exports = [
     children: [
       "/前端开发/vue/Vue3核心开发手册/使用Vite创建vue3项目",
       "/前端开发/vue/Vue3核心开发手册/setup语法糖之ref",
-      "/前端开发/vue/Vue3核心开发手册/setup语法糖之ref",
+      "/前端开发/vue/Vue3核心开发手册/setup语法糖之reactive",
     ]
   }
 ]
