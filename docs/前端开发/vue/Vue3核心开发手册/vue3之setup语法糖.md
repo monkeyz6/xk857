497d0a5e19c8e9896268cda38c7bd5c563ec2975
monkeyz6 <69353444+monkeyz6@users.noreply.github.com>
2022-08-02 19:45:13 +0800

    vue3之setup语法糖

---
+++ docs/前端开发/vue/Vue3核心开发手册/vue3之setup语法糖.md
@@ -0,0 +1,10 @@
+---
+title: vue3之setup语法糖
+date: 2021-08-02 19:45
+categories:
+- vue
+tags:
+- vue
+- vite
+---
+
--- docs/前端开发/vue/sidebar.js
+++ docs/前端开发/vue/sidebar.js
@@ -4,6 +4,7 @@ module.exports = [
     collapsable: false,
     children: [
       "/前端开发/vue/Vue3核心开发手册/使用Vite创建vue3项目",
+      "/前端开发/vue/Vue3核心开发手册/vue3之setup语法糖",
     ]
   }
 ]
