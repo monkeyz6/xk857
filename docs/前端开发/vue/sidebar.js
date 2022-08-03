e35531ed1e28ca64b462730faa432bdb10199281
monkeyz6 <69353444+monkeyz6@users.noreply.github.com>
2022-08-03 12:10:55 +0800

    vue引入第三方框架

--- docs/前端开发/vue/sidebar.js
+++ docs/前端开发/vue/sidebar.js
@@ -6,13 +6,18 @@ module.exports = [
       "/前端开发/vue/Vue3核心开发手册/使用Vite创建vue3项目",
       "/前端开发/vue/Vue3核心开发手册/setup语法糖之ref",
       "/前端开发/vue/Vue3核心开发手册/setup语法糖之reactive",
-      {title: "vue3的计算属性与侦听器", path: "/前端开发/vue/Vue3核心开发手册/setup语法糖之计算属性与侦听器"},
-      "/前端开发/vue/Vue3核心开发手册/vue3的生命周期函数",
+      {title: "Vue3的计算属性与侦听器", path: "/前端开发/vue/Vue3核心开发手册/setup语法糖之计算属性与侦听器"},
+      "/前端开发/vue/Vue3核心开发手册/Vue3的生命周期函数",
       "/前端开发/vue/Vue3核心开发手册/Vue3之父子组件的传参",
       "/前端开发/vue/Vue3核心开发手册/Vue3全局组件与局部组件",
       "/前端开发/vue/Vue3核心开发手册/Vue3之组件插槽",
       "/前端开发/vue/Vue3核心开发手册/Vue3异步组件&代码分包",
       "/前端开发/vue/Vue3核心开发手册/Vue3定义全局函数和变量",
+    ]
+  }, {
+    title: "vue引入第三方框架",
+    collapsable: false,
+    children: [
     ]
   }
 ]
