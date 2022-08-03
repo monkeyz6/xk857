b018267525e41be3abdc5496edfe7c8069d4ef6f
monkeyz6 <69353444+monkeyz6@users.noreply.github.com>
2022-08-03 22:21:34 +0800

    uniapp框架与资源路径

--- docs/前端开发/uniapp/sidebar.js
+++ docs/前端开发/uniapp/sidebar.js
@@ -4,6 +4,7 @@ module.exports = [
     collapsable: false,
     children: [
       "/前端开发/uniapp/uniapp常用API/入门与环境搭建",
+      "/前端开发/uniapp/uniapp常用API/uniapp框架与资源路径",
     ]
   }
 ]
---
+++ docs/前端开发/uniapp/uniapp常用API/uniapp框架与资源路径.md
@@ -0,0 +1,11 @@
+---
+title: uniapp框架与资源路径
+date: 2021-08-03 22:21
+categories:
+- 插件开发
+tags:
+- 插件开发
+---
+
+
+<!-- more -->
