f4ddd3c864cd1c0330434c89d6b72669cd97488f
monkeyz6 <69353444+monkeyz6@users.noreply.github.com>
2022-08-06 23:22:26 +0800

    动态路由的跳转与传参

--- docs/前端开发/vue/sidebar.js
+++ docs/前端开发/vue/sidebar.js
@@ -22,6 +22,7 @@ module.exports = [
       "/前端开发/vue/Pinia/pinia的基本使用",
       "/前端开发/vue/Pinia/VueRouter的安装及定义",
       "/前端开发/vue/Pinia/普通路由的跳转与传参",
+      "/前端开发/vue/Pinia/动态路由的跳转与传参",
     ]
   }, {
     title: "vue引入第三方框架",
