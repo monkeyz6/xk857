139a2014efaf1da137db87a47023e9233a94ba97
monkeyz6 <69353444+monkeyz6@users.noreply.github.com>
2022-08-07 17:03:00 +0800

    SpringBoot整合MybatisPlus

--- docs/.vuepress/sidebar.js
+++ docs/.vuepress/sidebar.js
@@ -3,6 +3,7 @@ module.exports = {
   '/后端开发/中间件/': require('../后端开发/中间件/sidebar.js'),
   '/后端开发/redis专栏/': require('../后端开发/redis专栏/sidebar.js'),
   '/后端开发/第三方支付/': require('../后端开发/第三方支付/sidebar.js'),
+  '/后端开发/常用框架/': require('../后端开发/常用框架/sidebar.js'),
   '/前端开发/前端基础/': require('../前端开发/前端基础/sidebar.js'),
   '/前端开发/vue/': require('../前端开发/vue/sidebar.js'),
   '/数据库/': require('../数据库/sidebar.js'),
